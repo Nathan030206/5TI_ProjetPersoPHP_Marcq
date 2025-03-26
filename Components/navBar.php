f0a401c0b2d4fce8e555875bc4dc2a632b6a6867
Nathan030206 <230064@site.asty-moulin.be>
2025-03-26 16:04:46 +0100

    Premier jet

---
+++ Components/navBar.php
@@ -0,0 +1,6 @@
+<ul class="flexible space-evenly">
+    <!-- grand écran -->
+    <li class="menu"><a href="index.php">Home</a></li>
+    <li  class="menu"><a href="inscription">Inscription</a></li>
+    <li  class="menu"><a href="connexion">Connexion</a></li>
+</ul>
---
+++ Views/pageAccueil.php
@@ -0,0 +1,49 @@
+
+<h1>Pages des créations de nos fidèles clients</h1>
+
+<div class="flexible wrap space-around">
+    <div class="border card">
+        
+        <div>
+            <h2><span>Création du pc des rêves de Amaury</span>  - <span>Amaury Aibach</span></h2>
+            <div class="flexible blocImageEcole"><img src="../Assets/Images/pc1.jpg" alt="itn" /></div>
+            <div class="center">
+            </div>
+        </div>
+    </div>
+    <div class="border card">
+        <h2 class="center">Création d'un monstre de compétition - Nathan Marcq</h2>
+        <div>
+            <div class="flexible blocImageEcole"><img src="../Assets/Images/pc2.jpg" alt="image lorem picsum" /></div>
+            <div class="center">
+                <div>
+                    <p> - RTX 5090 ti </p>
+                    <p> - Intel Core i9 14th 14900k</p>
+                    <p> - </p>
+                </div> 
+            </div>
+        </div>
+    </div>
+    <div class="border card">
+        <h2 class="center">Institut Saint Albert</h2>
+        <div>
+            <div class="flexible blocImageEcole"><img src="../Assets/Images/stAlbertIm.jpeg" alt="image lorem picsum" /></div>
+            <div class="center">
+                <p><span>Av Ferdinand Charlot 35</span> - <span>Jodoigne</span></p>
+                <h3>010811250</h3>
+                <a href="voirEcole.php" class="btn btn-page">Voir l'école</a>
+            </div>
+        </div>
+    </div>
+    <div class="border card">
+        <h2 class="center">Ecole de l'enfant Jésus</h2>
+        <div>
+            <div class="flexible blocImageEcole"><img src="../Assets/Images/enfantJesusIm.png" alt="image lorem picsum" /></div>
+            <div class="center">
+                <p><span>Rue de Sotriamont 1</span> - <span>Nivelles</span></p>
+                <h3>067893800</h3>
+                <a href="voirEcole.php" class="btn btn-page">Voir l'école</a>
+            </div>
+        </div>
+    </div>
+</div>
